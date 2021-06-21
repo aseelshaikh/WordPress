f78302c2c7aac80ca16d8c56748da95fccd9ea45
aseelshaikh <47882042+aseelshaikh@users.noreply.github.com>
2021-06-21 18:44:46 +0500

    updated2 for webapp

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'dawebapp-preview-database' );
+define( 'DB_NAME', 'webapp-preview-database' );
 
 /** MySQL database username */
 define( 'DB_USER', 'webapp-preview-server-admin' );
